77adc79964a097609f2d6600b880aa65172f7bb4
Andrew Genet <awg6@zips.uakron.edu>
2017-11-15 11:36:25 -0500

    Add project files.

---
+++ createSQL/Form1.Designer.cs
@@ -0,0 +1,375 @@
+﻿namespace createSQL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.displayQryBtn = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.swipeInput = new System.Windows.Forms.TextBox();
+            this.textClear = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.groupBox7 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox6.SuspendLayout();
+            this.groupBox7.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 19);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(709, 343);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(10, 29);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(697, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // displayQryBtn
+            // 
+            this.displayQryBtn.Location = new System.Drawing.Point(10, 56);
+            this.displayQryBtn.Name = "displayQryBtn";
+            this.displayQryBtn.Size = new System.Drawing.Size(106, 26);
+            this.displayQryBtn.TabIndex = 6;
+            this.displayQryBtn.Text = "Go";
+            this.displayQryBtn.UseVisualStyleBackColor = true;
+            this.displayQryBtn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(122, 56);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 26);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Clear Data";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(660, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "label2";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(745, 508);
+            this.tabControl1.TabIndex = 10;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.groupBox5);
+            this.tabPage1.Controls.Add(this.groupBox4);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(737, 482);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Console";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.groupBox7);
+            this.tabPage2.Controls.Add(this.groupBox6);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(737, 482);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Settings";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.Location = new System.Drawing.Point(6, 19);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(699, 381);
+            this.listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(705, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Log:";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.groupBox3);
+            this.tabPage3.Controls.Add(this.groupBox2);
+            this.tabPage3.Controls.Add(this.groupBox1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(737, 482);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Badge Example";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // swipeInput
+            // 
+            this.swipeInput.Location = new System.Drawing.Point(21, 29);
+            this.swipeInput.Name = "swipeInput";
+            this.swipeInput.Size = new System.Drawing.Size(686, 20);
+            this.swipeInput.TabIndex = 0;
+            // 
+            // textClear
+            // 
+            this.textClear.Tick += new System.EventHandler(this.textClear_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(162, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Extracted Employee Information: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(19, 58);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "label5";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(22, 58);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(686, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Location = new System.Drawing.Point(9, 197);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(721, 112);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "SQL";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(19, 31);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(132, 13);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Sample Generated Query: ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Location = new System.Drawing.Point(8, 91);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(721, 100);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Active Directory";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.swipeInput);
+            this.groupBox3.Location = new System.Drawing.Point(9, 18);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(720, 67);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Badge Swipe";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.comboBox1);
+            this.groupBox4.Controls.Add(this.displayQryBtn);
+            this.groupBox4.Controls.Add(this.label2);
+            this.groupBox4.Controls.Add(this.button3);
+            this.groupBox4.Location = new System.Drawing.Point(8, 6);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(721, 100);
+            this.groupBox4.TabIndex = 10;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Query SQL";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.dataGridView1);
+            this.groupBox5.Location = new System.Drawing.Point(8, 112);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(721, 362);
+            this.groupBox5.TabIndex = 11;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Data Viewer";
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.textBox1);
+            this.groupBox6.Location = new System.Drawing.Point(6, 6);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(723, 59);
+            this.groupBox6.TabIndex = 4;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "Server Location";
+            // 
+            // groupBox7
+            // 
+            this.groupBox7.Controls.Add(this.listBox1);
+            this.groupBox7.Location = new System.Drawing.Point(12, 71);
+            this.groupBox7.Name = "groupBox7";
+            this.groupBox7.Size = new System.Drawing.Size(717, 408);
+            this.groupBox7.TabIndex = 5;
+            this.groupBox7.TabStop = false;
+            this.groupBox7.Text = "Log";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 508);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Utility";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox6.ResumeLayout(false);
+            this.groupBox6.PerformLayout();
+            this.groupBox7.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button displayQryBtn;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TextBox swipeInput;
+        private System.Windows.Forms.Timer textClear;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.GroupBox groupBox7;
+    }
+}
+
---
+++ createSQL/Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Data.OleDb;
+using System.DirectoryServices;
+using System.DirectoryServices.ActiveDirectory;
+
+
+namespace createSQL
+{
+    // location of database files: C:\Program Files\Microsoft SQL Server\MSSQL14.SQLEXPRESS\MSSQL\DATA
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.Add("SELECT * FROM myTable;");
+            comboBox1.SelectedItem = "SELECT * FROM myTable;";
+            comboBox1.Items.Add("CREATE TABLE myTable ( primaryKey int IDENTITY(1,1) PRIMARY KEY, empId varchar(255) NOT NULL, lastName varchar(255), firstName varchar(255), mail varchar(255), title varchar(255), description varchar(255));");
+            comboBox1.Items.Add("DROP TABLE myTable;");
+            //comboBox1.Items.Add("INSERT INTO myTable (empId, lastName, firstName, mail, title, description) VALUES ('999999', 'Genet', 'Andrew', 'mymail@mail.com', 'Super cool dude', 'really super cool dude');");
+            comboBox1.Items.Add("SELECT COUNT(primaryKey) FROM myTable;");
+            label2.Text = "waiting...";
+            textBox1.Text = "Server=localhost\\SQLEXPRESS;Database=master;Trusted_Connection=True;";
+            //testDatabaseExists();
+
+            label5.Text = "";
+
+            groupBox4.Dock = DockStyle.Top;
+            groupBox5.Dock = DockStyle.Fill;
+            dataGridView1.Dock = DockStyle.Fill;
+            groupBox6.Dock = DockStyle.Top;
+            groupBox7.Dock = DockStyle.Fill;
+            listBox1.Dock = DockStyle.Fill;
+
+        }
+        
+        DataTable clearData = new DataTable();
+        ToolTip tt = new ToolTip();
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tt.SetToolTip(label2, "");
+            dataGridView1.DataSource = clearData;
+
+            SqlConnection myConn = new SqlConnection(textBox1.Text);
+
+            String qry = comboBox1.Text.ToString();
+
+            SqlCommand myCommand = new SqlCommand(qry, myConn);
+
+            try
+            {
+                myConn.Open();
+
+                //Use the above SqlCommand object to create a SqlDataReader object.
+                SqlDataReader rdr = myCommand.ExecuteReader();
+
+                listBox1.Items.Add("Executed Command: " + qry);
+                label2.Text = "Success";
+                label2.BackColor = Color.LightGreen;
+                timer1.Start();
+
+                if (!comboBox1.Items.Contains(qry))
+                {
+                    comboBox1.Items.Add(qry);
+                }
+                
+                //Use the DataTable.Load(SqlDataReader) function to put the results of the query into a DataTable.
+                DataTable dataTable = new DataTable();
+                dataTable.Load(rdr);
+                dataGridView1.DataSource = dataTable;
+            }
+            catch (System.Exception ex)
+            {
+                listBox1.Items.Add("Failed Command: " + qry);
+                label2.Text = "Failure";
+                label2.BackColor = Color.Pink;
+                tt.ShowAlways = false;
+                tt.SetToolTip(label2, ex.ToString());
+            }
+            finally
+            {
+                if (myConn.State == ConnectionState.Open)
+                {
+                    myConn.Dispose();
+                    myConn.Close();
+                }
+            } // end of try/catch
+        }// end display qry btn click
+
+        // extra stuff to make the badgeID part execute
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (swipeInput.TextLength > 34)
+            {
+                swipeInput.Enabled = false;
+                //parseData();
+                textClear.Start();
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        string empID = "null";
+        string lastName = "null";
+        string firstName = "null";
+        string mail = "null";
+        string title = "null";
+        string desc = "null";
+        string domain = "Domain: ";
+        string path = "LDAP://DC=,DC=,DC=,DC=";
+
+        // extract employee ID
+        //private void parseData()
+        //{
+        //    char[] delimiterChars = { '=', '?' };
+        //    string text = swipeInput.Text;
+        //    swipeInput.Clear();
+        //    string[] words = text.Split(delimiterChars);
+        //    int count = 0;
+
+        //    foreach (string s in words)
+        //    {
+        //        count++;
+        //        if (count == 3)
+        //        {
+        //            empID = s.ToString();
+        //        }
+        //    }
+        //    count = 0;
+        //    myData();
+        //    label5.Text = empID + ", " + lastName + ", " + firstName + ", " + mail + ", " + title + ", " + desc;
+        //    textBox2.Text = "INSERT INTO myTable (empId, lastName, firstName, mail, title, description) VALUES ('" + empID + "', '" + lastName + "', '" + firstName + "', '" + mail + "', '" + title + "', '" + desc + "');";
+            
+        //}
+
+        // search active directory
+        //private void myData()
+        //{
+        //    try
+        //    {
+        //        DirectoryEntry entry = new DirectoryEntry(path);
+
+        //        //search for a DirectoryEntry based on employeeID
+        //        DirectorySearcher search = new DirectorySearcher(entry);
+        //        search.Filter = "(employeeID=" + empID + ")";
+
+        //        SearchResult result = search.FindOne();
+
+        //        // if didnt find anything
+        //        if (result == null)
+        //        {
+        //            // and the path isnt lerner
+        //            if (path != "LDAP://DC=,DC=,DC=,DC=")
+        //            {
+        //                //close the open search
+        //                entry.Dispose();
+        //                entry.Close();
+
+        //                // change the path
+        //                path = "LDAP://DC=,DC=,DC=,DC=";
+        //                domain = "Domain: ";
+
+        //                // and try again
+        //                myData();
+        //                return;
+        //            }
+                    
+        //            entry.Dispose();
+        //            entry.Close();
+        //            return;
+        //        }
+        //        else
+        //        {
+        //            bool a = result.Properties.Contains("sn");
+        //            bool b = result.Properties.Contains("givenname");
+        //            bool c = result.Properties.Contains("mail");
+        //            bool d = result.Properties.Contains("title");
+        //            bool e = result.Properties.Contains("description");
+
+        //            if (a != false)
+        //            {
+        //                lastName = result.Properties["sn"][0].ToString();
+        //            }
+        //            if (b != false)
+        //            {
+        //                firstName = result.Properties["givenname"][0].ToString();
+        //            }
+        //            if (c != false)
+        //            {
+        //                mail = result.Properties["mail"][0].ToString();
+        //            }
+        //            if (d != false)
+        //            {
+        //                title = result.Properties["title"][0].ToString();
+        //            }
+        //            if (e != false)
+        //            {
+        //                desc = result.Properties["description"][0].ToString();
+        //            }
+        //        }
+
+        //        domain = "Domain: ";
+        //        path = "LDAP://DC=,DC=,DC=,DC=";
+        //        entry.Dispose();
+        //        entry.Close();
+        //        return;
+        //    }
+        //    catch
+        //    {
+
+        //    }
+        //}
+
+        // cosmetic solution to clearing out the data grid
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = clearData;
+        }
+
+        // turns the label back to normal
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            label2.Text = "waiting...";
+            label2.BackColor = Color.Empty;
+        }
+
+        //not sure yet if this works
+        public Boolean testDatabaseExists()
+        {
+            String connString = (textBox1.Text);
+            String cmdText = ("select * from master.dbo.sysdatabases");
+            Boolean bRet;
+
+            System.Data.SqlClient.SqlConnection sqlConnection = new System.Data.SqlClient.SqlConnection(connString);
+            System.Data.SqlClient.SqlCommand sqlCmd = new System.Data.SqlClient.SqlCommand(cmdText, sqlConnection);
+
+            try
+            {
+                sqlConnection.Open();
+                System.Data.SqlClient.SqlDataReader reader = sqlCmd.ExecuteReader();
+                bRet = reader.HasRows;
+                sqlConnection.Close();
+            }
+            catch (Exception e)
+            {
+                bRet = false;
+                sqlConnection.Close();
+                MessageBox.Show(e.Message);
+                return false;
+            } //End Try Catch Block
+
+            if (bRet == true)
+            {
+                MessageBox.Show("DATABASE EXISTS");
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("DATABASE DOES NOT EXIST");
+                return false;
+            } //END OF IF
+
+
+        } //END FUNCTION
+
+        private void textClear_Tick(object sender, EventArgs e)
+        {
+            swipeInput.Enabled = true;
+            swipeInput.Clear();
+        }
+    }// end class
+}// end namespace
